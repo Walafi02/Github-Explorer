fea88fda17e34668d2a5456b556c21bb6ebc5d92
Walafi02 <walafif@yahoo.com>
2020-04-27 16:50:19 +0000

    Updates

---
+++ precache-manifest.0e3ed586d16ecb0f57239979bc5d9ec6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a410b5d481bf783d7eea4460d489ea5",
+    "url": "/react-typescript/index.html"
+  },
+  {
+    "revision": "3a8e30ad87292d97cb03",
+    "url": "/react-typescript/static/js/2.c82b4f18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-typescript/static/js/2.c82b4f18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7972a7cea014d5aef19b",
+    "url": "/react-typescript/static/js/main.e1d0e835.chunk.js"
+  },
+  {
+    "revision": "6af0d247eb8e7bacc634",
+    "url": "/react-typescript/static/js/runtime-main.5760871f.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/react-typescript/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/react-typescript/static/media/logo.04f6798f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f30c3316821a64768f14921bf8de9731.js"
+  "/react-typescript/precache-manifest.0e3ed586d16ecb0f57239979bc5d9ec6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-typescript/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
