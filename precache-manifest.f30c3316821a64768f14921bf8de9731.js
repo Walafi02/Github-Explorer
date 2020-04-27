623b510c1a6bf704fade10bbb89997933c695e87
Walafi02 <walafif@yahoo.com>
2020-04-27 16:16:07 +0000

    Updates

---
+++ precache-manifest.f30c3316821a64768f14921bf8de9731.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb8889f25d14a8d624c4843ed44a74b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deaf4de435cf3adc9013",
+    "url": "/static/js/2.a92978a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a92978a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474e0f39f4f50b1e89bb",
+    "url": "/static/js/main.b8333513.chunk.js"
+  },
+  {
+    "revision": "c6a5e717b3706788d5b3",
+    "url": "/static/js/runtime-main.3d60ae38.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logo.04f6798f.svg"
+  }
+]);
