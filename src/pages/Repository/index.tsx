f003a6ac1256ef9d7498433f4ff94517a7265260
Walafi02 <walafif@yahoo.com>
2020-06-23 23:34:35 +0000

    finalizando o primeiro projeto com react e typescript

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 import { Header, RepositoryInfo, Issues } from './styles';
+import api from '../../service/api';
 
 interface PepositoryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
+
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<PepositoryParams>();
+
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then((response) => {
+      setRepository(response.data);
+    });
+
+    api.get(`repos/${params.repository}/issues`).then((response) => {
+      setIssues(response.data);
+    });
+  }, [params.repository]); //eslint-disable-line
 
   return (
     <>
@@ -20,45 +55,48 @@ const Repository: React.FC = () => {
           <FiChevronLeft size={16} /> Voltar
         </Link>
       </Header>
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
 
-      <RepositoryInfo>
-        <header>
-          <img
-            src="https://avatars2.githubusercontent.com/u/26489511?s=400&u=91dbf175d1ad5cd97f83a72a3a7890ce22cca10e&v=4"
-            alt="Walafi"
-          />
-          <div>
-            <strong>Faceboock/React</strong>
-            <p>Lorem, ipsum dolor sit amet laborum </p>
-          </div>
-        </header>
-
-        <ul>
-          <li>
-            <strong>1223</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>145</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>1223</strong>
-            <span>Issues abertas</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
-
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
       <Issues>
-        <Link to="hauhauhauh">
-          <div>
-            <strong>repository.full_name</strong>
-            <p>repository.description</p>
-          </div>
+        {issues.map((issue) => (
+          <a key={issue.id} href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </Link>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Issues>
+      ;
     </>
   );
 };
